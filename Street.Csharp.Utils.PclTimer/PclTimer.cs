627e1714e8431fb6c9efb85992f52a8e6c38d6d0
Kerry Street <kerrycstreet@gmail.com>
2013-11-06 21:24:01 -0800

    add IDisposable on PclTimer for _timer.Dispose

--- Street.Csharp.Utils.PclTimer/PclTimer.cs
+++ Street.Csharp.Utils.PclTimer/PclTimer.cs
@@ -1,5 +1,6 @@
 ﻿// this code is based on "Blakomen" (henry-chong.com)'s code in an answer to as SO question
 // http://stackoverflow.com/questions/12555049/timer-in-portable-library
+// and IDisposable from this SO answer: http://stackoverflow.com/questions/7485075/c-sharp-how-to-implement-dispose-method by PVitt
 // Per StackOverflow Licensing: http://meta.stackoverflow.com/questions/25956/what-is-up-with-the-source-code-license-on-stack-overflow
 // This particular Street.Csharp.Utils.PclTimer is licensed under a Creative Commons Attribution-ShareAlike 2.5 Generic License.
 // http://creativecommons.org/licenses/by-sa/2.5/legalcode
@@ -10,8 +11,10 @@ using System.Threading;
 
 namespace Street.Csharp.Utils
 {
-    public class PclTimer
+    public class PclTimer : IDisposable
     {
+        bool _disposed;
+
         private readonly Timer _timer;
 
         private readonly Action _action;
@@ -31,6 +34,26 @@ namespace Street.Csharp.Utils
         public bool Change(TimeSpan dueTime, TimeSpan period)
         {
             return _timer.Change(dueTime, period);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                    _timer.Dispose();
+                }
+            }
+            //dispose unmanaged resources
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
